b3a977fbab3cd08761ede30913595b904ce00a41
udcp <admin@udcp.net>
2012-03-12 16:46:18 +0900

    first commit

---
+++ map/overviewerConfig.js
@@ -0,0 +1,57 @@
+var overviewerConfig = {
+    "worlds": [
+        "world"
+    ], 
+    "tilesets": [
+        {
+            "last_rendertime": 1331532474, 
+            "name": "Overviewer Render (lighting)", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 8, 
+            "path": "world-lighting"
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "http://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "http://google-maps-icons.googlecode.com/files/home.png", 
+            "compass": "compass_upper-left.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1331534036"
+    }
+};
